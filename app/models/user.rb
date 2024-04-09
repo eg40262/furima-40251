05d188f65db88fea4801ba53aa630e92392f7879
eg40262 <eg40262@gmail.com>
2024-04-10 00:42:06 +0900

    ユーザー管理機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,22 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, 
+         :registerable,
+         :recoverable, 
+         :rememberable, 
+         :validatable
+
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,}+\z/i
+  validates :password, format: { with: VALID_PASSWORD_REGEX }
+
+  VALID_NAME_REGEX = /\A[\p{Han}\p{Hiragana}\p{Katakana}ー－]+\z/
+  validates :last_name, presence: true, format: { with: VALID_NAME_REGEX }
+  validates :first_name, presence: true, format: { with: VALID_NAME_REGEX }
+
+  VALID_KANA_REGEX = /\A[\p{Katakana}ー－]+\z/
+  validates :last_name_kana, presence: true, format: { with: VALID_KANA_REGEX }
+  validates :first_name_kana, presence: true, format: { with: VALID_KANA_REGEX }
+
+  validates :nickname, presence: true
+  validates :birth_date, presence: true
 end
+
