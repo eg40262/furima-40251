41b55d242500737c7f91ff2f986e847ea949c098
eg40262 <eg40262@gmail.com>
2024-04-12 22:32:14 +0900

    ログインしたら出品ページに飛べる

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,26 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+  
+  def new
+
+  end
+
+  def create
+    @item = item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
   def index
+
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :price) # ここにはItemモデルの属性を追加してください
   end
 end
