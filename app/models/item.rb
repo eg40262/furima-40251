aa4ef3c134c49b8565964347eae0b85719c3a6e5
eg40262 <eg40262@gmail.com>
2024-04-14 16:56:58 +0900

    ActiveHashの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,10 +1,17 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :sales_status
+  belongs_to :shipping_fee_status
+  belongs_to :prefecture
+  belongs_to :scheduled_delivery
+
   belongs_to :user
   has_one_attached :image
 
   validates :name, presence: true
   validates :info, presence: true
-  validates :category_id, presence: true
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :sales_status_id, presence: true
   validates :shipping_fee_status_id, presence: true
   validates :prefecture_id, presence: true
