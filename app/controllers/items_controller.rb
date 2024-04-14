2847193e4ba7b0a0df63678ee6015e587e295f69
eg40262 <eg40262@gmail.com>
2024-04-14 18:56:13 +0900

    出品情報入力の正誤判定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,11 +6,11 @@ class ItemsController < ApplicationController
   end
 
   def create
-    @item = Item.new(item_params)
+    @item = current_user.items.build(item_params)
     if @item.save
       redirect_to root_path
     else
-      render :new
+      render :new, status: :unprocessable_entity
     end
   end
 
@@ -21,6 +21,15 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:name, :description, :price, :image) 
+    params.require(:item).permit(:name, :info, :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, :price, :image) 
   end
 end
+
+  # def create
+    # @item = Item.new(item_params)
+    # if @item.save
+      # redirect_to root_path
+    # else
+      # render :new
+    # end
+  # end
--- app/models/item.rb
+++ app/models/item.rb
@@ -11,12 +11,12 @@ class Item < ApplicationRecord
 
   validates :name, presence: true
   validates :info, presence: true
-  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :sales_status_id, presence: true
-  validates :shipping_fee_status_id, presence: true
-  validates :prefecture_id, presence: true
-  validates :scheduled_delivery_id, presence: true
-  validates :price, presence: true
+  validates :category_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :sales_status_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipping_fee_status_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :prefecture_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :scheduled_delivery_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :price, presence: true,  numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
   validates :user, presence: true
   validates :image, presence: true
 end
