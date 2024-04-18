7c81090c0ddf727ab26eedb6566b60a4bf989624
eg40262 <eg40262@gmail.com>
2024-04-18 20:38:27 +0900

    出品者じゃないユーザーの編集アクセス禁止実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :check_user, only: [:edit]
 
   def new
     @item = Item.new
