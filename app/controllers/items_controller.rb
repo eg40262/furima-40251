bf872498b32992d410c0b80ab058be932013b6d1
eg40262 <eg40262@gmail.com>
2024-04-18 20:37:15 +0900

    変更するボタンと編集ページへの移行を実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,10 +22,31 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(@item)  # アイテムの詳細ページにリダイレクト
+    else
+      render :edit  # エラーがあった場合は再び編集ページを表示
+    end
+  end
+
   private
 
   def item_params
     params.require(:item).permit(:name, :info, :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id,
                                  :scheduled_delivery_id, :price, :image)
   end
+
+  def check_user
+    item = Item.find(params[:id])
+    if current_user.id != item.user_id
+      redirect_to root_path
+    end
+  end
+
 end
