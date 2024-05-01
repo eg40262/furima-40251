3051496c93e4ddad7977f1138f9dd2bfd4000db3
eg40262 <eg40262@gmail.com>
2024-05-01 23:12:25 +0900

    user_idとitem_idのvalidates追加

--- app/models/purchase_shipping.rb
+++ app/models/purchase_shipping.rb
@@ -4,6 +4,8 @@ class PurchaseShipping
 
   with_options presence: true do
     validates :token, presence: true
+    validates :user_id
+    validates :item_id
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
     validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
     validates :city
