ec647bf70f9b7817191e02dd72a88eb2a22195dd
eg40262 <eg40262@gmail.com>
2024-05-02 20:05:50 +0900

    不要なコメントアウトを削除

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -13,23 +13,3 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   }
 });
-
-
-
-
-// window.addEventListener('turbo:load', () => {
-      // const priceInput = document.getElementById("item-price");
-      // priceInput.addEventListener("input", () => {
-        // const inputValue = priceInput.value;
-        // const addTaxDom = document.getElementById("add-tax-price");
-        // const taxValue = Math.floor(inputValue * 0.1);
-        // addTaxDom.innerHTML = taxValue;
-    
-        // const profitDom = document.getElementById("profit");
-        // const profitValue = inputValue - taxValue;
-        // profitDom.innerHTML = profitValue;
-      // });
-    // });
-
-    
-    
