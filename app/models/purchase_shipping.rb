65651c847890a4ce931a653367a413f834d55b59
eg40262 <eg40262@gmail.com>
2024-05-02 15:25:18 +0900

    , presence: true削除、factoriesの修正

--- app/models/purchase_shipping.rb
+++ app/models/purchase_shipping.rb
@@ -3,7 +3,7 @@ class PurchaseShipping
   attr_accessor :token, :postal_code, :prefecture_id, :city, :street_address, :building_name, :phone_number, :user_id, :item_id
 
   with_options presence: true do
-    validates :token, presence: true
+    validates :token
     validates :user_id
     validates :item_id
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
--- spec/factories/purchase_shippings.rb
+++ spec/factories/purchase_shippings.rb
@@ -7,7 +7,5 @@ FactoryBot.define do
     street_address { '青山1-1-1' }
     building_name { '柳ビル103' }
     phone_number { '09012345678' }
-    user_id { 1 } # 実際には存在するユーザーIDを指定します
-    item_id { 1 } # 実際には存在するアイテムIDを指定します
   end
 end
